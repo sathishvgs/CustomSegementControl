c80a6b9016ea69ab821f8a0595324ece397c2c63
sathish kumar G <vgsathish1995@gmail.com>
2018-08-06 20:10:44 +0530

    Implemented swipe to change the segments

--- CustomSegmentControl/Classes/CustomSegmented.swift
+++ CustomSegmentControl/Classes/CustomSegmented.swift
@@ -13,6 +13,7 @@ public class CustomSegmented: UIView {
     private var textColor: UIColor = .black
     
     private var selectorView: UIView!
+    private var currentButtonTag: Int = 0
     
     weak public var onsegmentTapped: CallBackOnTapOfButton?
     
@@ -38,6 +39,7 @@ extension CustomSegmented {
         
         let buttonIndex = index.advanced(by: 0)
         let selectorXPosition = CGFloat(frame.width)/CGFloat(buttons.count) * CGFloat(buttonIndex)
+        currentButtonTag = sender.tag
         
         UIView.animate(withDuration: 0.3) {
             self.selectorView.frame.origin.x = selectorXPosition
@@ -63,6 +65,7 @@ extension CustomSegmented {
         self.createSegmentButtons()
         self.createSelectorView()
         self.setUpStackView()
+        self.swipeGestureConfiguration()
     }
     
     // Create the custom buttons inside the view
@@ -70,12 +73,14 @@ extension CustomSegmented {
         
         self.buttons.removeAll()
         
-        for buttonTitle in self.buttonTitles {
+        for (index,buttonTitle) in self.buttonTitles.enumerated() {
             
             let button: UIButton = UIButton(type: .system)
             button.setTitle(buttonTitle, for: .normal)
             button.setTitleColor(textColor, for: .normal)
             button.addTarget(self, action: #selector(onTapSegmentAction(sender:)), for: .touchUpInside)
+            print("INDEX for button tag is \(index)")
+            button.tag = index
             self.buttons.append(button)
         }
         self.buttons[0].setTitleColor(selectorColor, for: .normal)
@@ -105,4 +110,57 @@ extension CustomSegmented {
         stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
     }
+    
+}
+
+// MARK: Gesture Configuration
+extension CustomSegmented {
+    
+    private func swipeGestureConfiguration() {
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(notifySwipeGesture(gesture:)))
+        swipeRight.direction = .right
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(notifySwipeGesture(gesture:)))
+        swipeLeft.direction = .left
+        
+        guard let superView = self.superview else {
+            return
+        }
+        superView.addGestureRecognizer(swipeRight)
+        superView.addGestureRecognizer(swipeLeft)
+    }
+    
+    @objc private func notifySwipeGesture(gesture: UISwipeGestureRecognizer) {
+        
+        print("Gesture Direction is \(gesture.direction.rawValue)")
+        
+        switch gesture.direction.rawValue {
+        case 1:
+            swipeOnLeft()
+        case 2:
+            swipeOnRight()
+        default :
+            break
+        }
+    }
+    
+    private func swipeOnLeft() {
+        
+        guard (currentButtonTag > 0) else {
+            return
+        }
+        print("Swipe Left Tag \(currentButtonTag)")
+        self.onTapSegmentAction(sender: self.buttons[currentButtonTag - 1])
+    }
+    
+    private func swipeOnRight() {
+        
+        guard (currentButtonTag < (buttons.count - 1) ) else {
+            return
+        }
+        print("Swipe Right Tag \(currentButtonTag)")
+        self.onTapSegmentAction(sender: self.buttons[currentButtonTag + 1])
+    }
+    
 }
